735fbc148dc5aabd178e07738aabd65b47b5b937
yidiwang21 <ydwang21@gmail.com>
2018-05-12 20:06:25 -0700

    changed matlab file

---
+++ audio_analysis/spec_analysis.m
@@ -0,0 +1,20 @@
+%% spectrum analysis
+[y,fs] = audioread('output.wav');
+len1 = length(y) - 1;
+t = (0:1:len1)/fs;
+y = y(:,1);
+figure(1);
+subplot(2,1,1);
+plot(t,y);
+title('Original Signal in Time Domain');
+xlabel('Time');
+ylabel('Amplitude');
+
+yy = fft(y);
+yy = fftshift(yy);
+len2 = length(yy) - 1;
+subplot(2,1,2);
+plot(-pi:2*pi/len2:pi,abs(yy));
+title('Original Signal in Freq Domain');
+xlabel('\omega/\pi');
+ylabel('Amplitude');
