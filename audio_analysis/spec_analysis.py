b453e188a0d9cee498aea5498ce58e4e3f554389
yidiwang21 <ydwang21@gmail.com>
2018-05-24 16:18:14 -0700

    fixed freq order

--- audio_analysis/spec_analysis.py
+++ audio_analysis/spec_analysis.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 '''
 Created on May 20, 2018
-Description: plot basic spectrums of the audio file
+Description: basic processing of the audio file
 
 ffmpeg -i input.mp3 output.wav
 '''
@@ -10,8 +10,7 @@ import numpy as np
 import wave
 import sys
 from scipy.fftpack import fft, ifft, fftshift
-from scipy.signal import argrelextrema, find_peaks_cwt, general_gaussian, fftconvolve
-import scipy.signal
+from scipy.signal import argrelextrema, find_peaks_cwt
 from math import pi, log, floor, ceil
 
 import warnings
@@ -29,40 +28,38 @@ class calc():
 
     def getMaxRange(self, arr, f, interval):
         f_sort = f[arr.argsort()[::-1]]
-        print f_sort[0:20]
         ff = np.array([])
         for e in f_sort[0:20]:
             ff = np.append(ff, int(e / interval) * interval)
-        ff, idx = np.unique(ff, return_inverse = True)
-        return ff[idx]
+        _, idx = np.unique(ff, return_index = True)
+        return ff[np.sort(idx)]
 
-class AudioSpec():
+class AudioProcessing():
     def __init__(self, file):
         self.wf = wave.open(file, 'rb')
         self.data = self.wf.readframes(-1)
         self.data = np.frombuffer(self.data, 'Int16')
-        self.data = self.data / (2.**15)
+        self.data = self.data / (2.**15)    # normalize amplitude
         self.calc_ins = calc()
         self.fs = self.wf.getframerate()
 
     def time_spec(self):
         self.len1 = len(self.data)
         self.len1 = self.calc_ins.nextpow2(self.len1)
-#         self.t = np.arange(0, self.len1)
-#         self.t = self.t / self.wf.getframerate()
+        # self.t = np.arange(0, self.len1)
+        # self.t = self.t / self.wf.getframerate()
 
     def freq_spec(self):
         self.y = fft(self.data, self.len1)
         self.y = abs(self.y)
         self.y = self.y / float(self.len1)
         self.y = self.y ** 2
-#         self.y = fftshift(self.y)
-        len2 = len(self.y)
-        self.f = np.arange(0, len2) * self.fs / self.len1
+        # self.y = fftshift(self.y
+        self.f = np.arange(0, len(self.y)) * self.fs / self.len1
         # get envolope of the frequency waveform
         self.yy = self.y[1:int(floor(self.len1/2))]
         self.yy = self.yy[argrelextrema(self.yy, np.greater)[0]]
-        indexes = scipy.signal.argrelextrema(
+        indexes = argrelextrema(
             np.array(self.yy),
             comparator = np.greater,order = 500
             )
@@ -70,10 +67,9 @@ class AudioSpec():
         self.yy =  self.yy[indexes]
         self.len_env = len(self.yy)
         self.f_env = np.arange(0, self.len_env) * self.fs / self.len_env
-        # yy = self.f_env[self.yy.argsort()[::-1]]
         yy = self.calc_ins.getMaxRange(self.yy, self.f_env, 50)
+        print('Feature Frequencies:')
         print yy[0:10]
-
 
     def plot_spec(self):
         plt.figure(1)
@@ -85,7 +81,7 @@ class AudioSpec():
         plt.figure(2)
         plt.title('Waveform in Freq Domain')
         plt.plot(self.f[1:int(floor(self.len1/2))], self.y[1:int(floor(self.len1/2))])
-#         plt.plot(abs(self.y))
+        # plt.plot(abs(self.y))
         plt.xlabel('Frequency(Hz)')
         plt.ylabel('Amplitude')
 
@@ -96,7 +92,7 @@ class AudioSpec():
         plt.ylabel('Amplitude')
 
         plt.figure(4)
-        plt.title('Waveform in Freq Domain')
+        plt.title('Power of Frequency')
         plt.plot(self.f[1:int(floor(self.len1/2))], 10 * np.log10(self.y[1:int(floor(self.len1/2))]))
         plt.xlabel('Frequency(Hz)')
         plt.ylabel('Power(dB)')
@@ -106,11 +102,11 @@ class AudioSpec():
     def main(self):
         self.time_spec()
         self.freq_spec()
-        self.plot_spec()
+        # self.plot_spec()
 
 if __name__ == '__main__':
     try:
-        f = AudioSpec("../bee_sounds/bee_swarm_small.wav")
+        f = AudioProcessing("../bee_sounds/bee_swarm_small.wav")
         f.main()
     except KeyboardInterrupt:
         print('Terminated')
