37e9ca1ee23c2265e9473a770d434596dcf3b28d
yidiwang21 <ydwang21@gmail.com>
2018-05-25 15:40:38 -0700

    fix str

--- rpi/get_from_presistors.py
+++ rpi/get_from_presistors.py
@@ -21,8 +21,8 @@ def writeToFile():
     for cnt in range(0,6):
         data = bluetoothSerial.readline()
         print(str(data))
-        list.append(str(data))
-    fw.writerow([time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()), list])
+        list.append(data)
+    fw.writerow([time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()), str(list)])
     time.sleep(1)
 
 if __name__ == '__main__':
