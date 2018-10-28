fe770c7a04db2c1a5437f7eaad6e9fa4a4fab9bd
yidiwang21 <ydwang21@gmail.com>
2018-10-27 19:56:34 -0700

    add data collection

---
+++ data/ble_miss_rate.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+delay_between_sends = np.array([10, 15, 18, 20, 35, 45])
+transmision_time = np.array([89.4, 132.9, 159.1, 176.6, 307.7, 398.1])
+miss_rate = np.array([56.1, 34, 20.8, 11.9, 0.3, 0])
+
+
+fig, ax1 = plt.subplots()
+plt.title('Transmision Time vs. Miss Rate')
+
+ax1.set_xlabel('delay between sends (ms)')
+ax1.set_ylabel('transmision time (s)', color='tab:red')
+ax1.plot(delay_between_sends, transmision_time, color='tab:red')
+ax1.tick_params(axis='y', labelcolor='tab:red')
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+ax2.set_ylabel('miss rate (%)', color='tab:blue')  # we already handled the x-label with ax1
+ax2.scatter(delay_between_sends, miss_rate, color='tab:blue')
+ax2.tick_params(axis='y', labelcolor='tab:blue')
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.show()
