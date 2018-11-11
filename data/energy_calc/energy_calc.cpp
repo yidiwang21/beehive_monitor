cd353f41e2c927d6404a65de0822590570c9e765
yidiwang21 <ydwang21@gmail.com>
2018-11-10 23:25:37 -0800

    add fft

--- data/energy_calc/energy_calc.cpp
+++ data/energy_calc/energy_calc.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 #define SLEEPING_CURRENT    0.056   // mA
 #define RUNNING_CURRENT     25      // mA
-#define RUNNING_UNIT_TIME   0.93    // sending 1s audio file needs 1.5min, neglecting recording time        
+#define TRANSMIT_UNIT_TIME   0.93    // sending 1s audio file needs 1.5min, neglecting recording time        
 
 int main(int argc, char *argv[]) {
     /* if (argc != 5) {
@@ -20,18 +20,18 @@ int main(int argc, char *argv[]) {
     int sampling_rate = 0;
     int recording_time = 0;
     
-    while ((opt = getopt(argc, argv, "sr:")) != EOF) {
+    while ((opt = getopt(argc, argv, "s:r: ")) != EOF) {
         switch (opt)
         {
             case 's': 
-                sampling_rate = atoi(argv[idx+1]); idx = idx + 2; break;
-            case 'r': recording_time = atoi(argv[idx+1]); idx = idx + 2; break;
+                sampling_rate = atoi(optarg); break;
+            case 'r': recording_time = atoi(optarg); break;
             case '?': fprintf(stderr, "# Usage: ./energy_calc -s [sampling rate (times/h)] -r [recording time (s)]");
             default: cout<<endl; break;
         }
     }
 
-    float running_time = sampling_rate * RUNNING_UNIT_TIME * recording_time;  // minutes/hour
+    float running_time = sampling_rate * TRANSMIT_UNIT_TIME * recording_time;  // minutes/hour
     float running_energy_hr = running_time / 60 * RUNNING_CURRENT;
     float sleeping_energy_hr = (60 - running_time) / 60 * SLEEPING_CURRENT;
     float total_energy_month = (running_energy_hr + sleeping_energy_hr) * 24 * 30;
---
+++ rpi/freq.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import wave
+import sys
+import time
+import csv
+from scipy.fftpack import fft, ifft, fftshift
+from scipy.signal import argrelextrema, find_peaks_cwt
+from math import pi, log, floor, ceil
+
+def nextpow2(x):
+    if x == 0:
+        return 1
+    else:
+        return 2 ** (x - 1).bit_length()
+
+file = "../bee_sounds/bee_swarm_small.wav"
+
+wf = wave.open(file, 'rb')
+data = wf.readframes(-1)
+data = np.frombuffer(data, 'Int16') # FIXME: 32 
+data = data / (2.**15)  # 
+fs = wf.getframerate()
+
+len1 = len(data)
+len1 = nextpow2(len1)
+y = abs(fft(data, len1))
+y = y / float(len1)
+y = y ** 2
+f = np.arange(0, len(y)) * fs / len1
+
+plt.figure(1)
+plt.title('Waveform in Freq Domain')
+plt.plot(f[1:int(floor(len1/2))], y[1:int(floor(len1/2))])
+plt.xlabel('Frequency(Hz)')
+plt.ylabel('Amplitude')
+plt.show()
+
+
